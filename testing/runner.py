2dcca4d29daf192cae55a3b79241101b2e0d034a
Mohammad Malkawi <me@mohammadmalkawi.com>
2025-03-16 01:54:57 +0400

    Remove old ppt testing files

--- testing/runner.py
+++ testing/runner.py
@@ -9,33 +9,6 @@ if __name__ == '__main__':
         # (editor
         #  .get_slide(1)
         #  .search_and_comment(text="It worked!", text_to_highlight="document provides"))
-        # (editor
-        #  .get_slide(2)
-        #  .search_and_comment(text="It worked!", text_to_highlight="pwc"))
-        # (editor
-        #  .get_slide(4)
-        #  .search_and_comment(text="It worked!", text_to_highlight="culture of giving"))
-        # (editor
-        #  .get_slide(5)
-        #  .search_and_comment(text="It worked!", text_to_highlight="NPO services"))
-        # (editor
-        #  .get_slide(5)
-        #  .search_and_comment(text="It worked!", text_to_highlight="told us"))
-        # (editor
-        #  .get_slide(11)
-        #  .search_and_comment(text="It worked!", text_to_highlight="monitor water"))
-        # (editor
-        #  .get_slide(13)
-        #  .search_and_comment(text="It worked!", text_to_highlight="accelerators"))
-        # (editor
-        #  .get_slide(15)
-        #  .search_and_comment(text="It worked!", text_to_highlight="1,000"))
-        # (editor
-        #  .get_slide(15)
-        #  .search_and_comment(text="It worked!", text_to_highlight="1,000"))
-        # (editor
-        #  .get_slide(15)
-        #  .search_and_comment(text="It worked!", text_to_highlight="1,000"))
         editor.export("../testing/test.pptx")
 
 
